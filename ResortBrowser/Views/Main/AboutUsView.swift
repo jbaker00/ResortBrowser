4f0a3fa1051fa73bbbf4212cd97a718a02d67e5c
James Baker <jbaker00@gmail.com>
2022-09-19 19:08:18 -0700

    Refactored the file and function names for NewResort to Become About Me

--- ResortBrowser/Views/Main/AboutUsView.swift
+++ ResortBrowser/Views/Main/AboutUsView.swift
@@ -1,5 +1,5 @@
 //
-//  NewRecipieView.swift
+//  AboutUsView.swift
 //  ResortBrowser
 //
 //  Created by James Baker on 8/21/22.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct NewRecipieView: View {
+struct AboutUsView: View {
     var body: some View {
         NavigationView {
             Text("Global Vibes is an exclusive international travel agency.  Our Specialities are Restorts EVERYWHERE.  We have doen work in the Contentinal USA, Alaska, Hawaii, Eourpe, THe Caribeean, Asia, Australia, and Cruise Shops all over the world  For additional information please feel free to reach out to us at www.globalvibes.com or by emailing info@globalvibes.com.  We love to help those who do not have time or want to book their own travel.Dont forget to check us out on FB and IG")
@@ -18,8 +18,8 @@ struct NewRecipieView: View {
     }
 }
 
-struct NewRecipieView_Previews: PreviewProvider {
+struct AboutUsView_Previews: PreviewProvider {
     static var previews: some View {
-        NewRecipieView()
+        AboutUsView()
     }
 }
